d36fe3663e9db0bb260bf88de3aafde0c52086be
radioActive DROID <me@godwin.dev>
2023-05-23 23:20:59 +0100

    temp-commit

---
+++ src/aspnet/Feature/IModule.cs
@@ -0,0 +1,19 @@
+// Copyright 2022 - 2023 Godwin peter .O (me@godwin.dev)
+// 
+// Licensed under the MIT License;
+// you may not use this file except in compliance with the License.
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Proton.Common.AspNet.Feature;
+
+public interface IModule {
+    IServiceCollection RegisterModule(IServiceCollection services);
+    IEndpointRouteBuilder MapEndpoints(IEndpointRouteBuilder endpoints);
+}
