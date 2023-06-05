cefabbe03502a66c2525b146a51c9244ed687fdd
radioActive DROID <me@godwin.dev>
2023-06-05 13:15:48 +0100

    fix annotation namespace

--- src/standard/Response/AuditResponse.cs
+++ src/standard/Response/AuditResponse.cs
@@ -8,7 +8,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using Newtonsoft.Json;
+using System.Text.Json.Serialization;
 using Proton.Common.Interfaces;
 
 namespace Proton.Common.Response;
