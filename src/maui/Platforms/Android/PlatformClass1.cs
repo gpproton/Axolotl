6aa77e35478bd3fe94e29df433709c51690cd5b5
radioActive DROID <me@godwin.dev>
2023-10-04 00:43:00 +0100

    Removes MAUI

--- src/maui/Platforms/Android/PlatformClass1.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace Axolotl.Maui {
-    // All the code in this file is only included on Android.
-    public class PlatformClass1 {
-    }
-}
--- src/maui/Platforms/MacCatalyst/PlatformClass1.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace Axolotl.Maui {
-    // All the code in this file is only included on Mac Catalyst.
-    public class PlatformClass1 {
-    }
-}
